53446188495788bf91d3e5b16faaed4a48b83784
ShaneKent <sdkent@calpoly.edu>
2015-04-22 15:52:43 -0700

    Added the Point.java file. Basic version of Point.java file.

---
+++ Point.java
@@ -0,0 +1,20 @@
+public class Point{
+   private int x;
+   private int y;
+
+   public Point(int x, int y){
+      this.x = x;
+      this.y = y;
+   }
+
+   public double distance_sq(Point pt){
+      return Math.pow(this.getX() - pt.getX(), 2) + Math.pow(this.getY() - pt.getY(), 2);
+   }
+
+   public double adjacent(Point pt){
+      return ((this.getX() == pt.getX() && Math.abs(this.getY() - pt.getY()) == 1) ||
+              (this.getY() == pt.getY() && Math.abs(this.getX() - pt.getX()) == 1))
+
+   }
+}
+
