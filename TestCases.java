462480b9057e31f6c2115fb4fc76136f7d090028
ShaneKent <sdkent@calpoly.edu>
2015-04-27 18:20:16 -0700

    Added the initial base version of TestCases.java

---
+++ TestCases.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+import java.lang.Math;
+
+public class TestCases{
+   private static final double DELTA = 0.00001;
+
+   @Test
+   public void testExample(){
+      assertTrue(true);
+   }
+}
