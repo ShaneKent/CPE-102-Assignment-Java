7a06017f041b1cf72860a5bb9e0b91161558b411
ShaneKent <sdkent@calpoly.edu>
2015-04-27 18:15:38 -0700

    Added the MinerFull and MinerNotFull classes. Added nextPosition() method in Miner class.

---
+++ MinerFull.java
@@ -0,0 +1,33 @@
+public class MinerFull extends Miner{
+   
+   public MinerFull(String name, Object[] imgs, Point position, int rate, int resource_limit, int animation_rate){
+      super(name, imgs, position, rate, resource_limit, animation_rate);
+      this.setResourceCount(resource_limit);
+   }
+   
+   public Point[] minerToSmith(WorldModel world, Blacksmith smith){
+      Point e_pt = this.getPosition();
+      Point[] pt = new Point[1];
+      
+      if (smith == null){
+         pt[0] = e_pt;
+         return pt;
+      }
+
+      Point s_pt = smith.getPosition();
+      if (e_pt.adjacent(s_pt)){
+         smith.setResourceCount(smith.getResourceCount() + this.getResourceCount());
+         this.setResourceCount(0);
+         return pt;
+      }
+      else{
+         Point new_pt = this.nextPosition(world, s_pt);
+         return world.moveEntity(this, new_pt);
+      }
+   }
+
+   public Miner tryTransformMiner(WorldModel world){
+      Miner m = new MinerNotFull(this.getName(), this.getImages(), this.getPosition(), this.getRate(), this.getResourceLimit(), this.getAnimationRate());
+      return m;
+   }
+}
---
+++ MinerNotFull.java
@@ -0,0 +1,42 @@
+public class MinerNotFull extends Miner{
+   
+   public MinerNotFull(String name, Object[] imgs, Point position, int rate, int resource_limit, int animation_rate){
+      super(name, imgs, position, rate, resource_limit, animation_rate);
+   }
+
+   public String entityString(){
+      return "miner " + this.getName() + " " + this.getPosition().getX() + " " + this.getPosition().getY() +
+         " " + this.getResourceLimit() + " " + this.getRate() + " " + this.getAnimationRate();
+   }
+
+   public Point[] minerToOre(WorldModel world, Ore ore){
+      Point e_pt = this.getPosition();
+      Point[] pt = new Point[1];
+      if (ore == null){
+         pt[0] = e_pt;
+         return pt;
+      }
+
+      Point o_pt = ore.getPosition();
+      if (e_pt.adjacent(o_pt)){
+         this.setResourceCount(1 + this.getResourceCount());
+         //actions.remove_entity(world, ore);
+         pt[0] = o_pt;
+         return pt;
+      }else{
+         Point new_pt = this.nextPosition(world, o_pt);
+         return world.moveEntity(this, new_pt);
+      }
+   }
+
+   public Miner tryTransformMiner(WorldModel world){
+      if (this.getResourceCount() < this.getResourceLimit()){
+         return this;
+      }
+      else{
+         Miner m = new MinerFull(this.getName(), this.getImages(), this.getPosition(),
+              this.getRate(), this.getResourceLimit(), this.getAnimationRate());
+         return m;
+      }
+   }
+}
