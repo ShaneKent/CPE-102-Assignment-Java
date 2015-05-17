f132288c2e8852e33a15f7e7ae989d140fb0751d
sbilich <sbilich@calpoly.edu>
2015-05-17 12:44:46 -0700

    Add AnimatedActor and update Quake inheritance

---
+++ AnimatedActor.java
@@ -0,0 +1,16 @@
+public class AnimatedActor extends Occupant
+{
+
+   private int animation_rate;
+
+   public AnimatedActor(String name, List<PImage> imgs, Point position, int animation_rate){
+      super(name, imgs, position);
+      this.animation_rate = animation_rate;
+   }
+
+   public int getAnimationRate(){
+      return this.animation_rate;
+   }
+
+
+}
--- Quake.java
+++ Quake.java
@@ -1,15 +1,9 @@
 import processing.core.*;
 import java.util.List;
 
-public class Quake extends Occupant{
-   private int animation_rate;
-
+public class Quake extends AnimatedActor 
+{
    public Quake(String name, List<PImage> imgs, Point position, int animation_rate){
-      super(name, imgs, position);
-      this.animation_rate = animation_rate;
-   }
-
-   public int getAnimationRate(){
-      return this.animation_rate;
+      super(name, imgs, position, animation_rate);
    }
 }
