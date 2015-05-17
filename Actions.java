9da9e559cfed0ec8c99b59c548a7ae9a3046587f
sbilich <sbilich@calpoly.edu>
2015-05-17 10:49:56 -0700

    Update to Actions, add interface Act

--- Actions.java
+++ Actions.java
@@ -1,6 +1,5 @@
 public class Actions
-{
-    
+{    
     public static final int BLOB_RATE_SCALE = 4;
     public static final int BLOB_ANIMATION_RATE_SCALE = 50;
     public static final int BLOB_ANIMATION_MIN = 1;
@@ -19,30 +18,33 @@ public class Actions
     
     // empty constructor
     
-    public static createAnimationAction(TYPE world, Entity entity,
-                                        TYPE repeat_count)
+    public static createAnimationAction(WorldModel world, Entity entity,
+                                        int repeat_count)
     {
-        def action(current_ticks):
-        entity.removePendingAction(action);
-        
-        entity.nextImage();
-        
-        if (repeat_count != 1)
-        {
+      Act[] func = { null };
+      func[0] = (long current_ticks) -> {
+       entity.removePendingAction(action);
+
+       entity.nextImage();
+
+       if (repeat_count != 1)
+          { 
             scheduleAction(world, entity,
-                           createAnimationAction(world, entity, max(repeat_count - 1, 0)),
-                           current_ticks + entity.getAnimationRate());
-        }
-        
-        return [entity.getPosition()];
-        return action;
+               createAnimationAction(world, entity, max(repeat_count - 1, 0)),
+               current_ticks + entity.getAnimationRate());
+          }
+
+       return [entity.getPosition()];
+       };
+
+      return func[0];
     }
     
     
-    public static Entity tryTransformMiner(TYPE world, Entity entity,
-                                           TYPE transform)
+    public static Entity tryTransformMiner(WorldModel world, Entity entity,
+                                           Act transform)
     {
-        Entity new_entity = transform(world);
+        Entity new_entity = transform.FUNCTIONNAME(world);
         if (entity != new_entity)
         {
             clearPendingActions(world, entity);
@@ -54,16 +56,16 @@ public class Actions
     }
     
     
-    public static void removeEntity(TYPE world, Entity entity)
+    public static void removeEntity(WorldModel world, Entity entity)
     {
-        for (TYPE action : entity.getPendingActions())
+        for (Act action : entity.getPendingActions())
             world.unscheduleAction(action);
         entity.clearPendingActions();
         world.removeEntity(entity);
     }
     
-    public static OreBlob createBlob(TYPE world, String name, Point pt,
-                                     TYPE rate, TYPE ticks, TYPE i_store)
+    public static OreBlob createBlob(WorldModel world, String name, Point pt,
+                                     int rate, long ticks, ImageStore i_store)
     {
         OreBlob blob = new OreBlob(name, pt, rate,
                                    getImages(i_store, 'blob'),
@@ -74,8 +76,8 @@ public class Actions
     }
     
     
-    public static Ore createOre(TYPE world, String name, Point pt,
-                                TYPE ticks, TYPE i_store)
+    public static Ore createOre(WorldModel world, String name, Point pt,
+                                long ticks, ImageStore i_store)
     {
         Ore ore = new Ore(name, pt, getImages(i_store, 'ore'),
                           randint(ORE_CORRUPT_MIN, ORE_CORRUPT_MAX));
@@ -85,7 +87,7 @@ public class Actions
     }
     
     
-    public static Quake createQuake(TYPE world, Point pt, TYPE ticks, TYPE i_store)
+    public static Quake createQuake(WorldModel world, Point pt, long ticks, ImageStore i_store)
     {
         Quake quake = new Quake("quake", pt,
                                 getImages(i_store, 'quake'), QUAKE_ANIMATION_RATE);
@@ -94,8 +96,8 @@ public class Actions
     }
     
     
-    public static Vein createVein(TYPE world, String name, Point pt,
-                                  TYPE ticks, TYPE i_store)
+    public static Vein createVein(WorldModel world, String name, Point pt,
+                                  long ticks, ImageStore i_store)
     {
         Vein vein = new Vein("vein" + name,
                              randint(VEIN_RATE_MIN, VEIN_RATE_MAX),
@@ -104,27 +106,36 @@ public class Actions
     }
     
     
-    public static void scheduleAction(TYPE world, Entity entity,
-                                      TYPE action, long time)
+    public static void scheduleAction(WorldModel world, Entity entity,
+                                      Act action, long time)
     {
         entity.addPendingAction(action);
         world.scheduleAction(action, time);
     }
     
     
-    public static void scheduleAnimation(TYPE world, Entity entity, repeat_count=0)
+    public static void scheduleAnimation(WorldModel world, Entity entity, int repeat_count)
     {
         scheduleAction(world, entity,
                        createAnimationAction(world, entity, repeat_count),
                        entity.getAnimationRate());
     }
-    
-    
-    public static void clearPendingActions(world, Entity entity)
+
+      
+    public static void scheduleAnimation(WorldModel world, Entity entity)
     {
-        for (Action action : entity.getPendingActions())
+      int repeat_count = 0;
+      scheduleAction(world, entity,
+      createAnimationAction(world, entity, repeat_count),
+      entity.getAnimationRate());
+    }
+
+    
+    
+    public static void clearPendingActions(WorldModel world, Entity entity)
+    {
+        for (Act action : entity.getPendingActions())
             world.unscheduleAction(action);
         entity.clearPendingActions();
     }
-    
 }
