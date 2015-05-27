d1e14f312b069238e6856455b0d9d1700559cc86
Sara Bilich <sbilich@calpoly.edu>
2015-05-27 12:58:35 -0700

    Fix empty path problem

--- Miner.java
+++ Miner.java
@@ -31,7 +31,10 @@ public abstract class Miner extends Mover
    public Point nextPosition(WorldModel world, Point dest_pt){
       Point start_pt = this.getPosition();
       List<Node> path = AStar(world, world.getTileOccupant(dest_pt).getClass(), start_pt, dest_pt);
-      return path.get(1).pt;
+      if (!path.isEmpty())
+	  return path.get(1).pt;
+      else 
+	  return start_pt;
    }
    /*
    public Point nextPosition(WorldModel world, Point dest_pt){
